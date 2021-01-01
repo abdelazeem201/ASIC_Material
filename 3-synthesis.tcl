78eac1d77b3a75f198b62ed2f335da1067412e8e
Ahmed Abdelazeem <58098260+Casear98@users.noreply.github.com>
2021-01-01 11:08:45 +0200

    Update 3-synthesis.tcl

--- 3-synthesis.tcl
+++ 3-synthesis.tcl
@@ -1,9 +1,9 @@
 # design setup (change library files according to your technology)
-set link_library /home/eslam/Desktop/synopsys/uk65lscllmvbbr_120c25_tc.db
-set target_library /home/eslam/Desktop/synopsys/uk65lscllmvbbr_120c25_tc.db
+set link_library /home/abdelazeem/Desktop/synopsys/uk65lscllmvbbr_120c25_tc.db
+set target_library /home/abdelazeem/Desktop/synopsys/uk65lscllmvbbr_120c25_tc.db
 
 #analyze
-analyze -format verilog {/home/eslam/Desktop/synopsys/pfd_loopF.v}
+analyze -format verilog {/home/abdelazeem/Desktop/synopsys/pfd_loopF.v}
 
 #elaborate (pfd_loopF is the name of the top level module)
 elaborate pfd_loopF -architecture verilog -library DEFAULT -update
@@ -19,16 +19,16 @@ compile
 #export design (reports and netlist and timing files)
 write -hierarchy -format ddc -output /home/eslam/Desktop/synopsys/pfd_loopF_mapped.ddc
 
-report_constraint -nosplit -all_violators > /home/eslam/Desktop/synopsys/allviol.rpt
-report_area > /home/eslam/Desktop/synopsys/area.rpt
-report_timing > /home/eslam/Desktop/synopsys/timing.rpt
-report_resources -nosplit -hierarchy > /home/eslam/Desktop/synopsys/resources.rpt
-report_reference -nosplit -hierarchy > /home/eslam/Desktop/synopsys/references.rpt
+report_constraint -nosplit -all_violators > /home/abdelazeem/Desktop/synopsys/allviol.rpt
+report_area > /home/abdelazeem/Desktop/synopsys/area.rpt
+report_timing > /home/abdelazeem/Desktop/synopsys/timing.rpt
+report_resources -nosplit -hierarchy > /home/abdelazeem/Desktop/synopsys/resources.rpt
+report_reference -nosplit -hierarchy > /home/abdelazeem/Desktop/synopsys/references.rpt
 report_hierarchy > hierarchy.rpt
 report_design > design.rpt
 
 change_names -rules verilog -hierarchy -verbose
-write -hierarchy -format verilog -output /home/eslam/Desktop/synopsys/pfd_loopF_mapped.v
+write -hierarchy -format verilog -output /home/abdelazeem/Desktop/synopsys/pfd_loopF_mapped.v
 write_sdf -version 2.1 pfd_loopF_vlog.sdf
 write_sdc -nosplit pfd_loopF_vlog.sdc
 puts "Finished"
