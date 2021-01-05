b17d2560b4a6cf12d22a8d57b71d4648a8772915
Ahmed Abdelazeem <58098260+Casear98@users.noreply.github.com>
2021-01-05 20:03:47 +0200

    Create 0_readfile.tcl

---
+++ syn/0_readfile.tcl
@@ -0,0 +1,34 @@
+set TOP_DIR $TOPLEVEL
+set RPT_DIR report
+set NET_DIR netlist
+
+sh rm -rf ./$TOP_DIR
+sh rm -rf ./$RPT_DIR
+sh rm -rf ./$NET_DIR
+sh mkdir ./$TOP_DIR
+sh mkdir ./$RPT_DIR
+sh mkdir ./$NET_DIR
+
+# define a lib path here
+define_design_lib $TOPLEVEL -path ./$TOPLEVEL
+
+# Read Design File (add your files here)
+set HDL_DIR "../hdl"
+analyze -library $TOPLEVEL -format verilog "$HDL_DIR/"top_module".v \
+																						$HDL_DIR/"module_1".v \
+																						$HDL_DIR/"module_2".v \
+																						$HDL_DIR/"module_3".v \
+																						$HDL_DIR/"module_4".v \
+																						$HDL_DIR/"module_4".v"
+                                             
+
+# elaborate your design
+elaborate $TOPLEVEL -architecture verilog -library $TOPLEVEL
+
+# Solve Multiple Instance
+set uniquify_naming_style "%s_mydesign_%d"
+uniquify
+
+# link the design
+current_design $TOPLEVEL
+link
